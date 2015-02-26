c430f08f29737e11fa2813b204648aaa75b9ff95
Danielle <danielle@daniellemullane.com>
2015-02-25 21:20:31 -0700

    Add company show page, fix description not saving in reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,25 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-
-    def create
-
-      ### create a product here.
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Product has been successfully created."
+    else
+      render :new
     end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: "Product was successfully deleted."
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :company_id)
+  end
 
 end
